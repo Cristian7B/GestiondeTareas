e1a817fb774b03891c3691a9c9554ad9ba6a9aa9
Yisus <yisusMurci@.com>
2024-05-14 20:24:12 -0500

    Archivo c++

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*Sistema de gestión de tareas
+Estudiantes: Cristian Camilo Bonilla, Nicolas velsco y Jesús Antonio Murcia
+Fecha: 7 de julio*/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main (){
+    return 0;
+}
