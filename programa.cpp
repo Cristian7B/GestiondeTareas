6574965bcd060308c3965182884effd585ca0f43
Cristian <criscamcr7@gmail.com>
2024-05-19 09:28:00 -0500

    Add: First Functions

--- programa.cpp
+++ programa.cpp
@@ -1,3 +1,10 @@
+/*
+Sistema de gestión de tareas
+Estudiantes: Cristian Camilo Bonilla, Nicolas Velasco y Jesús Antonio Murcia
+Fecha: 5 de Junio
+*/
+
+
 #include <iostream>
 #include <locale.h>
 #include <string>
@@ -6,10 +13,10 @@ using namespace std;
 //Crear estudiante
 struct Estudiante 
 {
-    char nombre[1];
-    char codigo[1];
-    char apellidos[1];
-    char correo[1];
+    char nombre[100];
+    char codigo[100];
+    char apellidos[100];
+    char correo[100];
 };
 
 //Nombre de los cursos del estudiante
@@ -32,12 +39,12 @@ struct Tarea {
 };
 
 //Prototipado de funciones
-void crearEstudianteCursos(Estudiante &es);
+void crearEstudianteCursos(Estudiante &es, Cursos materias[], int contador);
 
 //Funciones de las tareas
 void crearTarea( Cursos &curso );
 void modificarTarea( Tarea &tarea );
-void eliminarTarea( Tarea &tarea );
+void eliminarTarea( Tarea &tarea, int contadorTarea );
 void estadoTarea( Tarea &tarea );
 
 //Consultas
@@ -46,10 +53,74 @@ void tareasFecha( Tarea &tarea);
 
 
 main() {
+    system("cls");
+    Tarea tareas[100];
+    Cursos materias[15];
+    Estudiante perfil;
+
+    int contadorTarea, contadorMateria = 0;
+
+    crearEstudianteCursos(perfil, materias, contadorMateria);
+
+}
+
+//Funciones
+void crearEstudianteCursos(Estudiante &perfil, Cursos materias[], int contador) {
+    int i;
+    string decision = "s";
+
+    cout << "Nombre del estudiante: ";
+    cin.getline(perfil.nombre, 100);
+
+    cin.clear();
+
+    cout << "Apellidos del estudiante: ";
+    cin.getline(perfil.apellidos, 100);
+
+    cout << "Código del estudiante: ";
+    cin.getline(perfil.codigo, 100);
+
+    cout << "Correo del estudiante: ";
+    cin.getline(perfil.correo, 100);
+
+    cout << perfil.nombre << endl;
+    cout << perfil.apellidos << endl;
+    cout << perfil.codigo << endl;
+    cout << perfil.correo << endl;
+
+    cout << "\nEl estudiante tiene un máximo de 15 materias para registrar.";
+
+    while( contador <15 && decision == "s") {
+        cout << "Nombre del curso " << contador + 1 << ": ";
+        cin.getline(materias[contador].nombre, 100);
+        cout << materias[contador].nombre << endl;
+
+        cin.clear();
+
+        cout << "Código del curso " << materias[contador].nombre << ": ";
+        cin.getline(materias[contador].codigo, 100);
+        cout << materias[contador].nombre;
+
+        cin.clear();
+        contador++;
+
+        do
+        {
+            cout << "\n¿Desea añadir otro curso?: s/n";
+            cin >> decision;
+        } while (decision != "s" && decision != "n");
+        
+
+        cin.ignore();
+    }
+
+    for ( i = 0; i < contador; i++) {
+        cout << materias[i].nombre << endl;
+        cout << materias[i].codigo << endl;
+    }
 
     
 }
-
 
 
 
