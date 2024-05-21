2ca5dfdb5327f158874326f4f7fc13d615116b71
Cristian <criscamcr7@gmail.com>
2024-05-21 14:35:03 -0500

    Fix: User experience, y cin.getline. Add: Menu y switch para el menú

--- programa.cpp
+++ programa.cpp
@@ -66,24 +66,80 @@ main() {
     Cursos materias[15];
     Estudiante perfil;
 
-    int contadorTarea, contadorMateria = 0;
+    int contadorTarea, contadorMateria = 0, opcion = 0;
 
-    cout << "Bienvenido a tu sistema gestor de tareas personal, es hora de resitrarte\n";
+    cout << "¡Bienvenido a tu sistema de gestion de tareas personal!\n";
+
+    system("PAUSE");
+    system("CLS");
+
+    cout << "Es hora de registar tu perfil.\n";
     registrarEstudiante(perfil);
 
+    //Menu de opciones
+    while (opcion == 0)
+    {    
+        system("CLS");      
+        cout << "Menú de tareas.\n";
+        cout << "\nEdición\n";
+        cout << "--------------------\n";
+        cout << "\t 1. Crear tarea.\n";    
+        cout << "\t 2. Crear tarea.\n";
+        cout << "\t 3. Modificar tarea.\n";
+        cout << "\t 4. Cambiar estado de tarea.\n";
+        cout << "\t 5. Eliminar tarea.\n";
+        cout << "\nConsultas\n";
+        cout << "--------------------\n";
+        cout << "\t 6. Consultar el historial de tareas de una materia.\n";
+        cout << "\t 7. Consultar tareas para una fecha específica.\n";
+        cout << "Salir del programa.\n";
+
+        cout << "\nOpcion: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            break;
+        
+        case 2:
+            break;
+        
+        case 3:
+            break;
+        
+        case 4:
+            break;
+        
+        case 5:
+            break;
+        
+        case 6:
+            break;
+        
+        case 7:
+            opcion = 1;
+            break;
+        
+        default:
+            break;
+        }
+    }
 }
 
 //Funciones
 void registrarEstudiante(Estudiante &estudiante){
     //Solicitar nombre, apellido y código
+    cout << "--------------------\n";
+    cout << "Datos personales.\n";
     do{
         cout << "Ingresa tu nombre: ";
-        cin >> estudiante.nombre;
+        cin.getline(estudiante.nombre, 100);
     }while(estudiante.nombre == NULL);
 
     do{
         cout << "Ingresa tu apellido: ";
-        cin >> estudiante.apellido;
+        cin.getline(estudiante.apellido, 100);
     }while(estudiante.apellido == NULL);
 
     do{
@@ -101,25 +157,28 @@ void registrarMaterias(Estudiante &estudiante){
     string continuar;
     int numMateriasRegistradas = 0;
 
-    cout << "Es momento de registrar tus materias: ";
+    cout << "\nMaterias.\n";
+    cout << "--------------------";
+    cout << "\nEs momento de registrar tus materias.\n";
 
     //Solicitar los datos de las materias inscritas
     while (continuar != "n" && numMateriasRegistradas < 15){
-        cout << "Ingresa el nombre de la materia: ";
+        cout << "\nIngresa el nombre de la materia: ";
         cin >> estudiante.materias[numMateriasRegistradas].nombre;
-        cout << "\nLa materia se registro como " << estudiante.materias[numMateriasRegistradas].nombre;
+        cout << "\tLa materia se registro como " << estudiante.materias[numMateriasRegistradas].nombre;
 
-        cout << "\nAhora registra el código de la materia: ";
+        cout << "\n\tAhora registra el código de la materia: ";
         cin >> estudiante.materias[numMateriasRegistradas].codigo;
 
         numMateriasRegistradas++;
 
         //Preguntar por si se quiere dejar de registrar materias
-        cout << "¿Desea inscribir más mateiras? s/n\n";
+        cout << "--------------------\n";
+        cout << "¿Desea inscribir más materias? s/n: ";
         cin >> continuar;
 
         while(continuar != "s" && continuar != "n"){
-            cout << "Digite 's' o 'n', ningún otro valor ";
+            cout << "Digite 's' o 'n', ningún otro valor: ";
             cin >> continuar;
         }
 
@@ -132,13 +191,13 @@ void registrarMaterias(Estudiante &estudiante){
 int obtenercodigo(){
     int codigo;
     
-    cout << "Ingrese el código de estudiante";
+    cout << "Ingrese el código de estudiante: ";
     cin >> codigo;
 
     while(cin.fail() || codigo < 0){
         cin.clear();
         cin.ignore();
-        cout << "Ingrese solamente valores númericos";
+        cout << "Ingrese solamente valores númericos: ";
         cin >> codigo;
     }
 
@@ -148,11 +207,10 @@ int obtenercodigo(){
 void crearEstudianteCursos(Estudiante &perfil, Cursos materias[], int contador) {
     int i;
     string decision = "s";
+    cin.ignore();
 
     cout << "Nombre del estudiante: ";
     cin.getline(perfil.nombre, 100);
-
-    cin.clear();
 
     cout << "Apellidos del estudiante: ";
     cin.getline(perfil.apellido, 100);
